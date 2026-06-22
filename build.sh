e7c89952d70f2fd99833a0eb21051cd036caa9eb
Your Name <your.email@gmail.com>
2026-06-22 09:40:57 +0530

    Remove build.sh, use render.yaml instead

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -o errexit
-
-apt-get update
-apt-get install -y tesseract-ocr libtesseract-dev
-
-pip install --upgrade pip
-pip install -r requirements.txt
-python manage.py collectstatic --no-input
